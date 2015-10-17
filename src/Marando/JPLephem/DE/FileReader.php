e8b6eb200b6c30eeee1c66d664a6c25b537c0560
Ashley Marando <a.marando@me.com>
2015-10-17 04:52:12 -0400

    add file structure

---
+++ src/Marando/JPLephem/DE/FileReader.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Marando\JPLephem\Results;
+
+class FileReader extends SplFileObject {
+
+  public function readBytes($byte1, $byteN) {
+
+  }
+
+  public function splitCurrent($delim = '', $trim = " \t\n\r\0\x0B") {
+
+  }
+
+}
---
+++ src/Marando/JPLephem/Results/Ephemeris.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Marando\JPLephem\Results;
+
+/**
+ * @property CartesianVector $position
+ * @property DE              $de
+ * @property SSObjBase       $center
+ * @property SSObjBase       $target
+ */
+class Ephemeris {
+
+}

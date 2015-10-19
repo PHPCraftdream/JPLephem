e81baab107fb5f65ed60dd1d9505147364b3bf7a
Ashley Marando <a.marando@me.com>
2015-10-18 20:18:51 -0400

    lunar libration

--- src/Marando/JPLephem/Moon.php
+++ src/Marando/JPLephem/Moon.php
@@ -3,6 +3,7 @@
 namespace Marando\JPLephem;
 
 use \Marando\JPLephem\SSObj;
+use \Marando\JPLephem\Results\Libration;
 
 class Moon extends SSObj {
 
@@ -12,6 +13,16 @@ class Moon extends SSObj {
 
   protected function getName() {
     return 'Moon';
+  }
+
+  public function libration() {
+    $libration = $this->interp(13, 3);
+    return Libration::rad($libration[0], $libration[1], $libration[2]);
+  }
+
+  public function mantleVelocity() {
+    return;
+    $velocity = $this->interp(14, 2);
   }
 
 }

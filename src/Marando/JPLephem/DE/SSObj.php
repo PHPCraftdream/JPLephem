c72143cb30f886ee7c8f83dc2308e1a8c84b2b0d
Ashley Marando <a.marando@me.com>
2015-11-04 03:48:01 -0500

    name

--- src/Marando/JPLephem/DE/SSObj.php
+++ src/Marando/JPLephem/DE/SSObj.php
@@ -23,61 +23,67 @@ namespace Marando\JPLephem\DE;
 class SSObj {
 
   public $id;
+  public $name;
 
-  public function __construct($id) {
-    $this->id = $id;
+  public function __construct($id, $name = null) {
+    $this->id   = $id;
+    $this->name = $name;
   }
 
   public static function SolarBary() {
-    return new static(0);
+    return new static(0, 'Solar System barycenter');
   }
 
   public static function Mercury() {
-    return new static(1);
+    return new static(1, 'Mercury');
   }
 
   public static function Venus() {
-    return new static(2);
+    return new static(2, 'Venus');
   }
 
   public static function Earth() {
-    return new static(301);
+    return new static(301, 'Earth');
   }
 
   public static function EarthBary() {
-    return new static(3);
+    return new static(3, 'Earth-Moon barycenter');
   }
 
   public static function Mars() {
-    return new static(4);
+    return new static(4, 'Mars');
   }
 
   public static function Jupiter() {
-    return new static(5);
+    return new static(5, 'Jupiter');
   }
 
   public static function Saturn() {
-    return new static(6);
+    return new static(6, 'Saturn');
   }
 
   public static function Uranus() {
-    return new static(7);
+    return new static(7, 'Uranus');
   }
 
   public static function Neptune() {
-    return new static(8);
+    return new static(8, 'Neptune');
   }
 
   public static function Pluto() {
-    return new static(9);
+    return new static(9, 'Pluto');
   }
 
   public static function Moon() {
-    return new static(10);
+    return new static(10, 'Moon');
   }
 
   public static function Sun() {
-    return new static(11);
+    return new static(11, 'Sun');
+  }
+
+  public function __toString() {
+    return $this->name ? $this->name : (string)$this->id;
   }
 
 }
